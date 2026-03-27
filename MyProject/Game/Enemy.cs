76891de9a7faa6279f89bd9f6e0917624632770f
Артём Резанцев <artom201523@gmail.com>
2026-03-27 11:29:54 +0300

    Init

---
+++ MyProject/Game/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Game
+{
+    internal abstract class Enemy : Entity
+    {
+        public int Armor;
+        public int Damage;
+        
+        
+    }
+}
---
+++ MyProject/Game/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MyProject.Game.Abilities;
+
+namespace MyProject.Game
+{
+    internal class Weapon
+    {
+        public string Name;
+        public int BaseDamage;
+    }
+}
